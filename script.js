fbef698e307bb3b6dc0723a7ace74874183ae795
thijs meijer <thijsmeijer.dev@gmail.com>
2024-09-06 14:12:54 +0200

    update script, added ability to follow a line indefinitely

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 basic.forever(function () {
     CutebotPro.trackbitStateValue()
     if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_12)) {
-        CutebotPro.pwmCruiseControl(100, 0)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_2) || CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_14)) {
-        CutebotPro.pwmCruiseControl(70, 0)
+        CutebotPro.pwmCruiseControl(45, 0)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_14)) {
+        CutebotPro.pwmCruiseControl(40, 0)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_13)) {
+        CutebotPro.pwmCruiseControl(35, 0)
     } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_8)) {
-        CutebotPro.pwmCruiseControl(0, 100)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_3) || CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_11)) {
-        CutebotPro.pwmCruiseControl(0, 70)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_1)) {
+        CutebotPro.pwmCruiseControl(0, 45)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_11)) {
+        CutebotPro.pwmCruiseControl(0, 40)
+    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_9)) {
+        CutebotPro.pwmCruiseControl(0, 35)
+    } else {
         CutebotPro.pwmCruiseControl(40, 40)
     }
 })
