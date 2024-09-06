9c496c9eb419d2ada14b91eb982a689e1097fd3e
thijs meijer <thijsmeijer.dev@gmail.com>
2024-09-06 15:17:11 +0200

    update script, added buttons to start a speedmode based on the thickness of the line

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-basic.forever(function () {
-    CutebotPro.trackbitStateValue()
-    if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_12)) {
-        CutebotPro.pwmCruiseControl(45, 0)
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffff00)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_14)) {
-        CutebotPro.pwmCruiseControl(40, 0)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_13)) {
-        CutebotPro.pwmCruiseControl(35, 0)
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x0000ff)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_8)) {
-        CutebotPro.pwmCruiseControl(0, 45)
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffff00)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_11)) {
-        CutebotPro.pwmCruiseControl(0, 40)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_9)) {
-        CutebotPro.pwmCruiseControl(0, 35)
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x0000ff)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_3)) {
-        CutebotPro.pwmCruiseControl(0, 25)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_2)) {
-        CutebotPro.pwmCruiseControl(25, 0)
-    } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_0)) {
-        CutebotPro.fullAstern()
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xff0000)
-    } else {
-        CutebotPro.pwmCruiseControl(35, 35)
-        CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x00ff00)
-    }
+let SpeedMode = 0
+input.onButtonPressed(Button.A, function () {
+    SpeedMode = 0
+    startRace();
 })
+input.onButtonPressed(Button.B, function () {
+    SpeedMode = 1
+    startRace();
+})
+
+function startRace() {
+    basic.forever(function () {
+        CutebotPro.trackbitStateValue()
+        if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_12)) {
+            CutebotPro.pwmCruiseControl(45, 0)
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffff00)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_14)) {
+            CutebotPro.pwmCruiseControl(40, 0)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_13)) {
+            CutebotPro.pwmCruiseControl(35, 0)
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x0000ff)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_8)) {
+            CutebotPro.pwmCruiseControl(0, 45)
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xffff00)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_11)) {
+            CutebotPro.pwmCruiseControl(0, 40)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_9)) {
+            CutebotPro.pwmCruiseControl(0, 35)
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x0000ff)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_3)) {
+            CutebotPro.pwmCruiseControl(0, 25)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_2)) {
+            CutebotPro.pwmCruiseControl(25, 0)
+        } else if (CutebotPro.getGrayscaleSensorState(TrackbitStateType.Tracking_State_0)) {
+            CutebotPro.fullAstern()
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xff0000)
+        } else {
+            if (SpeedMode == 1) {
+                CutebotPro.pwmCruiseControl(35, 35)
+            } else {
+                CutebotPro.pwmCruiseControl(45, 45)
+            }
+            CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x00ff00)
+        }
+    })
+}
